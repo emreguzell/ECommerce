f20b4154bfb56067baeef39c306ae9ccc8287a98
Emre GÜZEL <eguzel9@ford.com.tr>
2023-04-18 01:31:36 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            // Additional configurations...
+        }
+
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Price { get; set; }
+        public string ImageUrl { get; set; }
+
+        // Foreign key for the Category
+        public int CategoryId { get; set; }
+
+    }
+}
